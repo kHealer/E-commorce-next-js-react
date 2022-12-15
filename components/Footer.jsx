304db8b762c87d8eee7a500e3f4b20c6c5cf51e4
Safa HELVACI <95585412+kHealer@users.noreply.github.com>
2022-12-15 05:31:22 +0300

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {AiFillInstagram,AiOutlineTwitter} from 'react-icons/ai'
+
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      2022 kH Headphones All rights reserved
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+
+  )
+}
+
+export default Footer
---
+++ sanity_ecommorce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'zxtq80lm',
+    dataset: 'production'
+  }
+})
